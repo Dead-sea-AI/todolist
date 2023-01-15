32d24a8d7b2f1ed715997ae92bcbd4c3f712692f
Andrew <andrew@airyx.dev>
2023-01-15 19:56:53 +0300

    init project

---
+++ src/views/ToDoListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    test
+    <TestComponent></TestComponent>
+  </div>
+</template>
+
+<script>
+import TestComponent from "@/components/TestComponent.vue";
+export default {
+  components: {
+    TestComponent,
+  },
+};
+</script>
+
+<style></style>
